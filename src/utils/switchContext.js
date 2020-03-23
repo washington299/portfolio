f45e5c9ffea8e5d38c5d194b243bd45b9133c98d
Washington Campos <washingtoncampos9@gmail.com>
2020-03-23 02:20:18 -0300

    feat: create switchContext

---
+++ src/utils/switchContext.js
@@ -0,0 +1,19 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const ThemeContext = createContext('light');
+
+export default function ThemeProvider({ children }) {
+  const [colorTheme, setColorTheme] = useState('dark');
+
+  return (
+    <ThemeContext.Provider value={{ colorTheme, setColorTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  const { colorTheme, setColorTheme } = context;
+  return { colorTheme, setColorTheme };
+}
