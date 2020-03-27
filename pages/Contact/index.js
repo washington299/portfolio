b3dd426e48641264557fe94b552a55c1d503dc1e
Washington Campos <washingtoncampos9@gmail.com>
2020-03-27 11:51:20 -0300

    feat: add validation to the fields

--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -1,54 +1,87 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 import Global from '../../src/components/SwitchProvider';
 import Layout from '../../src/components/Layout';
 import Header from '../../src/components/Header';
 import Footer from '../../src/components/Footer';
 
-import { PageStyle, Title, ContentArea } from '../../src/utils/globalStyles';
+import {
+  PageStyle,
+  Title,
+  ContentArea,
+  Error,
+} from '../../src/utils/globalStyles';
 import { Styles, Form } from './styles';
 
-const Contact = () => (
-  <Global>
-    <Layout title="Contact me">
-      <PageStyle>
-        <Header />
-        <Styles>
-          <Title>Contact Me</Title>
-          <ContentArea style={{ justifyContent: 'center' }}>
-            <Form>
-              <div className="form-area">
-                <div className="form--input-area">
-                  <label htmlFor="name">Name:</label>
-                  <input type="text" name="name" id="name" placeholder="Nome" />
+const Contact = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  function submit(data) {
+    console.log(data);
+  }
+
+  return (
+    <Global>
+      <Layout title="Contact me">
+        <PageStyle>
+          <Header />
+          <Styles>
+            <Title>Contact Me</Title>
+            <ContentArea style={{ justifyContent: 'center' }}>
+              <Form>
+                <div className="form-area">
+                  <div className="form--input-area">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      placeholder="Nome"
+                      ref={register({ required: true })}
+                    />
+                    {errors.name && errors.name.type === 'required' && (
+                      <Error>Campo obrigatório!</Error>
+                    )}
+                  </div>
+                  <div className="form--input-area">
+                    <label htmlFor="email">E-mail:</label>
+                    <input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Seu melhor e-mail"
+                      ref={register({ required: true })}
+                    />
+                    {errors.email && errors.email.type === 'required' && (
+                      <Error>Campo obrigatório!</Error>
+                    )}
+                  </div>
+                  <div className="form--input-area">
+                    <label htmlFor="textarea">Mensagem:</label>
+                    <textarea
+                      type="text"
+                      name="textarea"
+                      id="textarea"
+                      placeholder="Digite sua mensagem aqui..."
+                      ref={register({ required: true })}
+                    />
+                    {errors.textarea && errors.textarea.type === 'required' && (
+                      <Error>Campo obrigatório!</Error>
+                    )}
+                    <button type="submit" onClick={handleSubmit(submit)}>
+                      Enviar
+                    </button>
+                  </div>
                 </div>
-                <div className="form--input-area">
-                  <label htmlFor="email">E-mail:</label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Seu melhor e-mail"
-                  />
-                </div>
-                <div className="form--input-area">
-                  <label htmlFor="textarea">Mensagem:</label>
-                  <textarea
-                    type="text"
-                    name="textarea"
-                    id="textarea"
-                    placeholder="Digite sua mensagem aqui"
-                  />
-                  <button type="submit">Enviar</button>
-                </div>
-              </div>
-            </Form>
-          </ContentArea>
-        </Styles>
-        <Footer />
-      </PageStyle>
-    </Layout>
-  </Global>
-);
+              </Form>
+            </ContentArea>
+          </Styles>
+          <Footer />
+        </PageStyle>
+      </Layout>
+    </Global>
+  );
+};
 
 export default Contact;
