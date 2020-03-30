9e9953a189502ba20d7f4c4571bc0c208a7f4c20
Washington Campos <washingtoncampos9@gmail.com>
2020-03-30 05:41:13 -0300

    refactor: create a folder for the Themes

---
+++ src/utils/Themes/dark.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'dark',
+  colors: {
+    font: {
+      primary: '#FFFFFF',
+      secondary: '#00FFFF',
+    },
+    background: {
+      primary: '#2F3136',
+      secondary: '#202225',
+      tertiary: '#00000F',
+    },
+  },
+};
---
+++ src/utils/Themes/light.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'light',
+  colors: {
+    font: {
+      primary: '#000000',
+      secondary: '#1e90ff',
+    },
+    background: {
+      primary: '#ecf2f8',
+      secondary: '#FFFFFF',
+      tertiary: '#dce7f2',
+    },
+  },
+};
