acea267caec3768e044f50d7988c25ce3144c2af
Washington Campos <washingtoncampos9@gmail.com>
2020-04-02 03:26:33 -0300

    refactor: add new logic to toggle dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 import { ThemeProvider } from 'styled-components';
+
+import { parseCookies } from '../src/utils/parseCookies';
 
 import Sidebar from '../src/components/Sidebar';
 import Header from '../src/components/Header';
@@ -10,16 +13,19 @@ import light from '../src/utils/Themes/light';
 import dark from '../src/utils/Themes/dark';
 
 import SidebarProvider from '../src/utils/Contexts/menuContext';
-import useStorageTheme from '../src/utils/useStorageTheme';
 
 import GlobalStyle from '../src/utils/Styles/Global';
 import { PagePosition } from '../src/utils/Styles/Elements';
 
-function MyApp({ Component, pageProps }) {
-  const [theme, setTheme] = useStorageTheme('theme', light);
+function MyApp({ Component, pageProps, storagedDarkMode = false }) {
+  const [darkMode, setDarkMode] = useState(() => JSON.parse(storagedDarkMode));
+
+  useEffect(() => {
+    Cookies.set('darkMode', JSON.stringify(darkMode));
+  }, [darkMode]);
 
   function toggleTheme() {
-    setTheme(theme.title === 'light' ? dark : light);
+    setDarkMode(!darkMode);
   }
 
   return (
@@ -27,13 +33,13 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Portfólio</title>
       </Head>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
+      <ThemeProvider theme={darkMode ? dark : light}>
         <>
+          <GlobalStyle />
           <SidebarProvider>
             <Sidebar />
             <PagePosition>
-              <Header toggleTheme={toggleTheme} />
+              <Header toggleTheme={toggleTheme} checked={darkMode} />
               <Component {...pageProps} />
               <Footer />
             </PagePosition>
@@ -42,6 +48,14 @@ function MyApp({ Component, pageProps }) {
       </ThemeProvider>
     </>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const cookies = parseCookies(req);
+
+  return {
+    props: { storagedDarkMode: cookies.darkMode },
+  };
 }
 
 export default MyApp;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,28 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Switch from 'react-switch';
-import { ThemeContext } from 'styled-components';
 
 import MenuMobile from '../MenuMobile';
 
 import { HeaderStyle, SwitchIcon, ColorText } from './styles';
 
-const Header = ({ toggleTheme }) => {
-  const { title } = useContext(ThemeContext);
-
-  return (
-    <HeaderStyle>
-      <MenuMobile />
-      <SwitchIcon>
-        <ColorText>Light</ColorText>
-        <Switch
-          onChange={toggleTheme}
-          checked={title !== 'light'}
-          uncheckedIcon={false}
-          checkedIcon={false}
-          offColor="#FFFFFF"
-          onColor="#000000"
-          offHandleColor="#000000"
-          onHandleColor="#FFFFFF"
-        />
-        <ColorText>Dark</ColorText>
-      </SwitchIcon>
-    </HeaderStyle>
-  );
-};
+const Header = ({ toggleTheme, checked }) => (
+  <HeaderStyle>
+    <MenuMobile />
+    <SwitchIcon>
+      <ColorText>Light</ColorText>
+      <Switch
+        onChange={toggleTheme}
+        checked={checked}
+        uncheckedIcon={false}
+        checkedIcon={false}
+        offColor="#FFFFFF"
+        onColor="#000000"
+        offHandleColor="#000000"
+        onHandleColor="#FFFFFF"
+      />
+      <ColorText>Dark</ColorText>
+    </SwitchIcon>
+  </HeaderStyle>
+);
 
 export default Header;
