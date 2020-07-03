14c5b5bd9e73c3094a1c245e43d9da31a8f5fdd2
Washington Campos <washingtoncampos9@gmail.com>
2020-07-03 03:08:55 -0300

    refactor: divide each type of data in separate const

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -1,92 +1,92 @@
-export default {
-  projectsData: [
-    {
-      id: 1,
-      img: '/assets/projects/react-test-vagaJr.png',
-      title: 'React teste vaga Jr',
-      link: 'https://github.com/washington299/react-test-DevJr',
-    },
-    {
-      id: 2,
-      img: '/assets/projects/nodejs-express-mongodb.jpeg',
-      title: 'NodeJs clean-architecture',
-      link: 'https://github.com/washington299/nodeJs-clean-architecture',
-    },
-    {
-      id: 3,
-      img: '/assets/projects/docker-node.png',
-      title: 'NodeJs + typescript + docker',
-      link: 'https://github.com/washington299/nodejs-typescript-docker',
-    },
-    {
-      id: 4,
-      img: '/assets/projects/nodejs-express-mongodb.jpeg',
-      title: 'Classificados backend',
-      link: 'https://github.com/washington299/classificados-backend',
-    },
-    {
-      id: 5,
-      img: '/assets/projects/classificados.png',
-      title: 'Classificados frontend',
-      link: 'https://github.com/washington299/classificados-frontend',
-    },
-    {
-      id: 6,
-      img: '/assets/projects/naruto.png',
-      title: 'Naruto Template',
-      link: 'https://github.com/washington299/naruto-template',
-    },
-    {
-      id: 7,
-      img: '/assets/projects/react-node.jpg',
-      title: 'CRUD React+Node',
-      link: 'https://github.com/washington299/crud-with-ReactJs-and-NodeJs',
-    },
-  ],
-  skillsData: [
-    {
-      id: 1,
-      img: '/assets/skills/html5-e-css3.jpg',
-      title: 'HTML5 e CSS3',
-      link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
-    },
-    {
-      id: 2,
-      img: '/assets/skills/javascript.jpg',
-      title: 'Javascript',
-      link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
-    },
-    {
-      id: 3,
-      img: '/assets/skills/reactJs.jpg',
-      title: 'ReactJs',
-      link: 'https://reactjs.org/',
-    },
-    {
-      id: 4,
-      img: '/assets/skills/nextJs.png',
-      title: 'NextJs',
-      link: 'https://nextjs.org/',
-    },
-    {
-      id: 5,
-      img: '/assets/skills/node-mongodb.jpg',
-      title: 'NodeJs e MongoDB',
-      link: 'https://nodejs.org/en/',
-    },
-    {
-      id: 6,
-      img: '/assets/skills/typescript.jpg',
-      title: 'Typescript',
-      link: 'https://www.typescriptlang.org/',
-    },
-  ],
-  studyData: [
-    {
-      id: 1,
-      img: '/assets/skills/postgresql.jpeg',
-      title: 'PostgreSQL',
-      link: 'https://www.postgresql.org/',
-    },
-  ],
-};
+export const projectsData = [
+  {
+    id: 1,
+    img: '/assets/projects/react-test-vagaJr.png',
+    title: 'React teste vaga Jr',
+    link: 'https://github.com/washington299/react-test-DevJr',
+  },
+  {
+    id: 2,
+    img: '/assets/projects/nodejs-express-mongodb.jpeg',
+    title: 'NodeJs clean-architecture',
+    link: 'https://github.com/washington299/nodeJs-clean-architecture',
+  },
+  {
+    id: 3,
+    img: '/assets/projects/docker-node.png',
+    title: 'NodeJs + typescript + docker',
+    link: 'https://github.com/washington299/nodejs-typescript-docker',
+  },
+  {
+    id: 4,
+    img: '/assets/projects/nodejs-express-mongodb.jpeg',
+    title: 'Classificados backend',
+    link: 'https://github.com/washington299/classificados-backend',
+  },
+  {
+    id: 5,
+    img: '/assets/projects/classificados.png',
+    title: 'Classificados frontend',
+    link: 'https://github.com/washington299/classificados-frontend',
+  },
+  {
+    id: 6,
+    img: '/assets/projects/naruto.png',
+    title: 'Naruto Template',
+    link: 'https://github.com/washington299/naruto-template',
+  },
+  {
+    id: 7,
+    img: '/assets/projects/react-node.jpg',
+    title: 'CRUD React+Node',
+    link: 'https://github.com/washington299/crud-with-ReactJs-and-NodeJs',
+  },
+];
+
+export const skillsData = [
+  {
+    id: 1,
+    img: '/assets/skills/html5-e-css3.jpg',
+    title: 'HTML5 e CSS3',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/HTML',
+  },
+  {
+    id: 2,
+    img: '/assets/skills/javascript.jpg',
+    title: 'Javascript',
+    link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript',
+  },
+  {
+    id: 3,
+    img: '/assets/skills/reactJs.jpg',
+    title: 'ReactJs',
+    link: 'https://reactjs.org/',
+  },
+  {
+    id: 4,
+    img: '/assets/skills/nextJs.png',
+    title: 'NextJs',
+    link: 'https://nextjs.org/',
+  },
+  {
+    id: 5,
+    img: '/assets/skills/node-mongodb.jpg',
+    title: 'NodeJs e MongoDB',
+    link: 'https://nodejs.org/en/',
+  },
+  {
+    id: 6,
+    img: '/assets/skills/typescript.jpg',
+    title: 'Typescript',
+    link: 'https://www.typescriptlang.org/',
+  },
+];
+
+export const studyData = [
+  {
+    id: 1,
+    img: '/assets/skills/postgresql.jpeg',
+    title: 'PostgreSQL',
+    link: 'https://www.postgresql.org/',
+  },
+];
